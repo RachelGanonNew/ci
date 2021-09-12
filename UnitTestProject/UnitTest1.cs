ad311e70d95ca5e88a9aaea9d1d8e05ca3bf55cd
RachelGanonNew <r05458410430@gmail.com>
2021-09-12 11:34:50 +0300

    kk

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sandbox;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 c = new Class1();
+            int res = c.add(2, 3);
+                Assert.AreEqual(res, 0);
+          
+   
+
+        }
+    }
+}
